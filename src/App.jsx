7cadc0022f8163e42760dafd437c28768c9b8c0f
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-23 20:07:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 import CreateAccount from "./pages/CreateAccount";
 
 function App() {
+  useEffect(() => {
+    const clearSessionOnClose = () => {
+      localStorage.removeItem("userType");
+      localStorage.removeItem("roll");
+    };
+
+    window.addEventListener("beforeunload", clearSessionOnClose);
+
+    return () => {
+      window.removeEventListener("beforeunload", clearSessionOnClose);
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
