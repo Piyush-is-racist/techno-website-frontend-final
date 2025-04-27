549fccb5a26bf15eff6e34e135e57574cfddd349
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-27 15:19:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path'; // <-- ADD THIS
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      pages: path.resolve(__dirname, 'src/pages'),
+      components: path.resolve(__dirname, 'src/components'),
+      assets: path.resolve(__dirname, 'src/assets'),
+      // add more if you have more folders
+    },
+  },
 });
